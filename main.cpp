20d3b2c3c7e66ea10876c07970caa5f648aa2022
Krzysztof Szczepański <54471767+krzysztof01-sz@users.noreply.github.com>
2022-01-12 20:05:46 +0100

    Add a new speaker to dialogue

--- main.cpp
+++ main.cpp
@@ -44,6 +44,11 @@ int main()
     igor2.addDialogueLine("DUPA DUPA DUPA");
     igor2.addDialogueLine("LA LA LA DUPA DUPA");
 
+    std::string guest1 = "Nos z sąsiedztwa";
+    Speaker krzychu( guest1, texturesManager.get("hero") );
+    krzychu.addDialogueLine("JA CI DAM DUPE DUPE");
+    krzychu.addDialogueLine("LAAA KIEDY MNIE WYPUSCISZ Z PIWNICY??");
+
     Dialogue dialogue(window, fontsManager.get("marrada"));
     dialogue.addSpeaker(igor);
     dialogue.addSpeaker(igor2);
